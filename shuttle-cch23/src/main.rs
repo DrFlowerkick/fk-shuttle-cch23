3e225b2d6503457bfb1c061df28dd627c502c4d2
DrFlowerkick <flowerkick14@gmail.com>
2023-12-01 21:16:02 +0100

    Added fake_error to challenge -1

--- shuttle-cch23/src/main.rs
+++ shuttle-cch23/src/main.rs
@@ -1,12 +1,18 @@
-use axum::{routing::get, Router};
+use axum::{routing::get, Router, response::IntoResponse, http::StatusCode};
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
 }
 
+async fn fake_error() -> impl IntoResponse {
+    StatusCode::INTERNAL_SERVER_ERROR
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
+    let router = Router::new()
+        .route("/", get(hello_world))
+        .route("/-1/error", get(fake_error));
 
     Ok(router.into())
 }
